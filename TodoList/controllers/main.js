083ce579a7ed4b4a6fa9c1a262db63417edac1f2
LeoRoma <xiajtn@gmail.com>
2020-02-14 12:03:06 +0000

    main.js updated with users function
    Co-authored-by: jaitone <jaitoone@gmail.com>

--- TodoList/controllers/main.js
+++ TodoList/controllers/main.js
@@ -1,48 +1,98 @@
 const getTableData = (req, res, db) => {
   db.select('*').from('testtable1')
     .then(items => {
-      if(items.length){
+      if (items.length) {
         res.json(items)
       } else {
-        res.json({dataExists: 'false'})
+        res.json({ dataExists: 'false' })
       }
     })
-    .catch(err => res.status(400).json({dbError: 'db error'}))
+    .catch(err => res.status(400).json({ dbError: 'db error' }))
 }
 
 const postTableData = (req, res, db) => {
   const { first, last, email, phone, location, hobby } = req.body
   const added = new Date()
-  db('testtable1').insert({first, last, email, phone, location, hobby, added})
+  db('testtable1').insert({ first, last, email, phone, location, hobby, added })
     .returning('*')
     .then(item => {
       res.json(item)
     })
-    .catch(err => res.status(400).json({dbError: 'db error'}))
+    .catch(err => res.status(400).json({ dbError: 'db error' }))
 }
 
 const putTableData = (req, res, db) => {
   const { id, first, last, email, phone, location, hobby } = req.body
-  db('testtable1').where({id}).update({first, last, email, phone, location, hobby})
+  db('testtable1').where({ id }).update({ first, last, email, phone, location, hobby })
     .returning('*')
     .then(item => {
       res.json(item)
     })
-    .catch(err => res.status(400).json({dbError: 'db error'}))
+    .catch(err => res.status(400).json({ dbError: 'db error' }))
 }
 
 const deleteTableData = (req, res, db) => {
   const { id } = req.body
-  db('testtable1').where({id}).del()
+  db('testtable1').where({ id }).del()
     .then(() => {
-      res.json({delete: 'true'})
+      res.json({ delete: 'true' })
     })
-    .catch(err => res.status(400).json({dbError: 'db error'}))
+    .catch(err => res.status(400).json({ dbError: 'db error' }))
 }
+
+// SIGNUP
+
+const getUserData = (req, res, db) => {
+  db.select('*').from('users')
+    .then(items => {
+      if (items.length) {
+        res.json(items)
+      } else {
+        res.json({ dataExists: 'false' })
+      }
+    })
+    .catch(err => res.status(400).json({ dbError: 'db error' }))
+}
+
+const postUserData = (req, res, db) => {
+  const { username, email, password, date_created, last_login } = req.body
+  const date_created = new Date()
+  db('users').insert({ username, email, password, date_created, last_login })
+    .returning('*')
+    .then(item => {
+      res.json(item)
+    })
+    .catch(err => res.status(400).json({ dbError: 'db error' }))
+}
+
+const putUserData = (req, res, db) => {
+  const { uid, username, email, password } = req.body
+  db('users').where({ uid }).update({ username, email, password })
+    .returning('*')
+    .then(item => {
+      res.json(item)
+    })
+    .catch(err => res.status(400).json({ dbError: 'db error' }))
+}
+
+const deleteUserData = (req, res, db) => {
+  const { uid } = req.body
+  db('users').where({ id }).del()
+    .then(() => {
+      res.json({ delete: 'true' })
+    })
+    .catch(err => res.status(400).json({ dbError: 'db error' }))
+}
+
 
 module.exports = {
   getTableData,
   postTableData,
   putTableData,
-  deleteTableData
+  deleteTableData,
+  // Signup
+  getUserData,
+  postUserData,
+  putUserData,
+  deleteUserData
 }
--- TodoList/schema.sql
+++ TodoList/schema.sql
@@ -8,3 +8,13 @@ CREATE TABLE testtable1 (
  hobby VARCHAR(100),
  added TIMESTAMP NOT NULL
 );
+
+CREATE TABLE users (
+  uid SERIAL PRIMARY KEY,
+  username VARCHAR(255) UNIQUE,
+  email VARCHAR(255),
+  password VARCHAR(255),
+  email_verified BOOLEAN,
+  date_created DATE,
+  last_login DATE
+);
